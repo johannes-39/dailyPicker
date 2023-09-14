1969d309f63c64dd2c1e68ea3412fd4a2cf29c90
JST <johannes.staedtler@iso-gruppe.com>
2023-09-14 13:13:18 +0200

    coding kata startview

--- pages/DailyPicker/dailyPicker.tsx
+++ pages/DailyPicker/dailyPicker.tsx
@@ -1,8 +1,5 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Container from "@mui/material/Container";
 import {useState} from "react";
-import {Grid} from "@mui/material";
 
 interface Artist {
     id: number;
@@ -24,53 +21,36 @@ const DailyPicker: React.FC<any> = () => {
         setClickedRandom(true);
     }
 
-
-
     return(
-        <Container maxWidth="lg">
-            <Box sx={{border: 'solid', height: '100%'}}>
-                <Box sx={{border: 'solid', width: '50%'}}><h1>Teilnehmer:</h1></Box>
-                <Grid container spacing={0}>
-                        <Box sx={{border: 'solid', height: '500px', width: '50%'}}>
-                        <input
-                            value={name}
-                            onChange={e => setName(e.target.value)}
-                        />
-                        {artists.map(artist => (
-                            <li key={artist.id}>{artist.name} <button onClick={() => {
-                                setArtists(
-                                    artists.filter(a =>
-                                        a.id !== artist.id
-                                    )
-                                );
-                            }}>
-                                Delete
-                            </button></li>
-                        ))}
-                    </Box>
-                    <Box sx={{border: 'solid', height: '500px', width: '50%'}}><h3>Gewonnen hat:</h3><Box>
-                        {artists.length > 0 && clickedRandom ?
-                            <div>
-                                {artists[randomNumber].name}</div> : null
-                        }
-                    </Box></Box>
+        <div>
+            <h1>Teilnehmer:</h1>
+            <input
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+            {artists.map(artist => (
+                <li key={artist.id}>{artist.name} <button onClick={() => {
+                    setArtists(
+                        artists.filter(a =>
+                            a.id !== artist.id
+                        )
+                    );
+                }}>
+                    Delete
+                </button></li>
+            ))}<h3>Gewonnen hat:</h3>
+            {artists.length > 0 && clickedRandom ?
+                <div>
+                    {artists[randomNumber].name}</div> : null
+            }
+            <button onClick={() => {
+                setArtists([
+                    ...artists,
+                    { id: nextId++, name: name }
+                ]);
+            }}>Hinzufügen</button><button className='ui button' onClick={generateRandomNumber}>Mischen</button>
+        </div>
 
-
-
-                </Grid>
-
-
-
-                <Box sx={{border: 'solid', width: '100%', height: '100px'}}><button onClick={() => {
-                    setArtists([
-                        ...artists,
-                        { id: nextId++, name: name }
-                    ]);
-                }}>Hinzufügen</button><button className='ui button' onClick={generateRandomNumber}>Mischen</button></Box>
-            </Box>
-
-
-        </Container>
     )
 }
 export default DailyPicker;
